c582af5466977fd79db603b5128dd8edeef7b4df
Kerthin <belichenko.roden@list.ru>
2018-10-13 19:20:44 +0300

    Create a drop-down menu based on the library anime.js

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,21 @@
 //= ../../bower_components/jquery/dist/jquery.min.js
 
 /********************
+	anime.min.js
+********************/
+//= partials/libs/anime.min.js
+
+/********************
+	revealer.js
+********************/
+//= partials/libs/revealer.js
+
+/********************
+	popupMenu.js
+********************/
+//= partials/popupMenu.js
+
+/********************
 	loader.js
 ********************/
 //= partials/loader.js
---
+++ app/js/partials/popupMenu.js
@@ -0,0 +1,37 @@
+(function() {
+	document.documentElement.className = 'js';
+	var formEl = document.querySelector('.menuPopup'),
+		revealer = new RevealFx(formEl),
+		openCtrl = document.querySelector('.js-burger_popupOpen');
+
+		openCtrl.onclick = function() {
+			if (openCtrl.classList.contains('js-burger_popupOpen') === true) {
+				revealer.reveal({
+					bgcolor: '#fd5f00',
+					direction: 'tb',
+					duration: 700,
+					onCover: function(contentEl, revealerEl) {
+						formEl.classList.add('js-menuPopup_open');
+						contentEl.style.opacity = 1;
+						openCtrl.classList.remove('js-burger_popupOpen');
+						openCtrl.style.zIndex = "999999";
+						formEl.style.zIndex = "50";
+						contentEl.style.zIndex = "50";
+					}
+				});
+			} else {
+				formEl.classList.remove('js-menuPopup_open');
+				revealer.reveal({
+					bgcolor: '#fd5f00',
+					direction: 'bt',
+					duration: 700, 
+					onCover: function(contentEl, revealerEl) {
+						formEl.classList.remove('js-menuPopup_open');
+						contentEl.style.opacity = 0;
+						openCtrl.classList.add('js-burger_popupOpen');
+					}
+				});
+			}
+		}
+	formEl.addEventListener('submit', function(ev) {ev.preventDefault();});
+})();
