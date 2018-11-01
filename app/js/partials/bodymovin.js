76ca4c028e414b157e2f05a0816adac0ff8a3bb5
Kerthin <belichenko.roden@list.ru>
2018-11-01 22:39:50 +0300

    Upload folder 'docs'

--- app/js/partials/bodymovin.js
+++ app/js/partials/bodymovin.js
@@ -3,5 +3,5 @@ var animation = lottie.loadAnimation({
 	renderer: 'svg',
 	loop: true,
 	autoplay: true,
-	path: '../../image/BurstElementZigZag.json'
+	path: '../image/BurstElementZigZag.json'
 });
